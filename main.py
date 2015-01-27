3f4b069d2b5c810f0cca139def255fd8de99a56b
NickLi <wslg1010@163.com>
2015-01-27 17:40:20 +0800

    add unittest

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 #coding:utf-8
 __author__ = 'lg'
+import unittest
 from selenium import webdriver
 from pageObject import loginPage
 from pageObject import mainPage
 import time
-driver = webdriver.Chrome()
 
-driver.get('http://192.168.0.236')
-driver.maximize_window()
-login = loginPage.loginPage(driver)
+class CMCCCloud(unittest.TestCase):
+    def setUp(self):
+        #pass
+        driver = webdriver.Chrome()
+        driver.get('http://192.168.0.236')
+        #窗口最大化
+        driver.maximize_window()
+        self._driver = driver
 
-login.username('admin')
-login.password('asdf123')
-login.logon()
+    def tearDown(self):
+        time.sleep(5)
+        self._driver.quit()
 
-#driver.switch_to.window(driver.window_handles[-1])
-mPage = mainPage.mainPage(driver)
-
-mPage.importDeviceManual()
-time.sleep(5)
-
-driver.quit()
+    def test_login(self):
+        login = loginPage.loginPage(self._driver)
+        login.username('admin')
+        login.password('asdf123')
+        login.logon()
+        keyword = self._driver.find_element_by_class_name('li_login_wrap').text
+        self.assertEqual(keyword,u'admin，您好！  [退出]')
+    #driver.switch_to.window(driver.window_handles[-1])
+    # main = mainPage.mainPage(driver)
+    #
+    # main.importDeviceManual()
+if __name__ =='__main__':
+    print 'start test...'
+    unittest.main()
